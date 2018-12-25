18f8b60fcb2f248abdbc88c4f3f75e1f4af04ac7
alexnathanson <alex@alexnathanson.com>
2018-12-24 21:15:11 -0500

    even cleaner

--- minimalmodbusTest2.py
+++ minimalmodbusTest2.py
@@ -1,18 +1,18 @@
 import minimalmodbus
 import serial
 
-usbDevice = '/dev/ttyUSB0'
+usbDevice = 'COM3'
 
 modbusSlaveID = 1
 
 # can be 'ascii' or 'rtu'
 modbusFormat = 'rtu'
 
-#in hexadecimal its 0x3100
-registerToRead = 12544
+#in decimal 12544 or in hexadecimal its 0x3100 (use decimal)
+registerToRead = 12548
 
 # 3 is for Holding Registers, 4 is for Input Registers
-functionCode = 3
+functionCode = 4
 
 # initialize the device
 device = minimalmodbus.Instrument(usbDevice, modbusSlaveID, modbusFormat)
@@ -25,4 +25,4 @@ device.serial.parity = serial.PARITY_NONE
 device.serial.stopbits = 1
 device.serial.timeout = 2   # seconds
 
-print device.read_register(registerToRead, functioncode=functionCode)
+print device.read_register(registerToRead, 2, functioncode=functionCode)
